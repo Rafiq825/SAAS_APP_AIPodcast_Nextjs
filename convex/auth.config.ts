90d1757349fdb5fbfb32672f6aa38aede339dd07
Muhammad Rafiq <rafkhan9323@gmail.com>
2024-06-03 15:17:11 +0500

    follow along the tutorial

---
+++ convex/auth.config.ts
@@ -0,0 +1,10 @@
+const authConfig = {
+    providers: [
+      {
+        domain: "https://thankful-redbird-18.clerk.accounts.dev",
+        applicationID: "convex",
+      },
+    ]
+  };
+  
+  export default authConfig;
